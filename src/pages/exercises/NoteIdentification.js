f57a732c960b66e9ea781044da3711886f26ffde
ChrisFala <christopherfala42@gmail.com>
2022-07-10 09:32:23 -0700

    CSS Grid Testing

--- src/pages/exercises/NoteIdentification.js
+++ src/pages/exercises/NoteIdentification.js
@@ -3,12 +3,14 @@ import trebleclef from '../../images/Music Note 59.svg'
 import bassclef from '../../images/Music Note 5.svg'
 import sharp from '../../images/Music Note 57.svg'
 import flat from '../../images/Music Note 1.svg'
+import staff from '../../images/Music Note 68.svg'
 import './exercise.css'
 
 const NoteIdentificaiton = () => {
     return (
         <div id="note-identification" className="display-note">
             <div id="note-display">
+                <img className="staff" src={staff} alt=""/>
                 <img className="wholenote" src={wholenote} alt="" />
                 <img className="trebleclef" src={trebleclef} alt="" />
                 <img className="bassclef" src={bassclef} alt="" />
