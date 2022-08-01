2c946f4ba0e1c9facd3891c7b24ec3dab87234d9
ChrisFala <christopherfala42@gmail.com>
2022-07-31 19:19:34 -0700

    Initialized Canvas & drawImage()

--- src/pages/exercises/NoteIdentification.js
+++ src/pages/exercises/NoteIdentification.js
@@ -5,10 +5,35 @@ import sharp from '../../images/Music Note 57.svg'
 import flat from '../../images/Music Note 1.svg'
 import staff from '../../images/Music Note 68.svg'
 import './exercise.css'
+import { useRef } from 'react'
+import { useEffect } from 'react'
 
 const NoteIdentificaiton = () => {
+    const myCanvas = useRef();
+
+    useEffect(() => {
+        const context = myCanvas.current.getContext("2d");
+        const image = new Image();
+        image.src=staff;
+        image.onload = () => {
+            context.drawImage(
+                image, -100, -100,280, 280
+                );
+        };
+    }, []);
+
+    const correctAnswer = ()=>{
+
+    }
+
+    const wrongAnswer = ()=> {
+
+    }
     return (
         <div id="note-identification" className="display-note">
+            <canvas ref={myCanvas} id="note-display-canvas">
+                
+            </canvas>
             <div id="note-display">
                 <img className="staff" src={staff} alt=""/>
                 <img className="wholenote" src={wholenote} alt="" />
