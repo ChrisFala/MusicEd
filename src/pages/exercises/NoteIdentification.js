f1080d69118228cbe5ad2c0dd5c09a3af7d53e6f
ChrisFala <christopherfala42@gmail.com>
2022-08-14 17:10:45 -0700

    Canvas Image Layering

--- src/pages/exercises/NoteIdentification.js
+++ src/pages/exercises/NoteIdentification.js
@@ -3,7 +3,7 @@ import trebleclef from '../../images/Music Note 59.svg'
 import bassclef from '../../images/Music Note 5.svg'
 import sharp from '../../images/Music Note 57.svg'
 import flat from '../../images/Music Note 1.svg'
-import staff from '../../images/Music Note 68.svg'
+import staff from '../../images/Staff Test.svg'
 import './exercise.css'
 import { useRef } from 'react'
 import { useEffect } from 'react'
@@ -13,29 +13,39 @@ const NoteIdentificaiton = () => {
 
     useEffect(() => {
         const context = myCanvas.current.getContext("2d");
-        const image = new Image();
-        image.src=staff;
-        image.onload = () => {
+        const musicStaff = new Image();
+        musicStaff.src = staff;
+        musicStaff.onload = () => {
             context.drawImage(
-                image, -100, -100,280, 280
-                );
+                musicStaff, 0, 0
+            );
         };
     }, []);
+    useEffect(() => {
+        const context = myCanvas.current.getContext("2d");
+        const note = new Image();
+        note.src = wholenote;
+        note.onload = () => {
+            context.drawImage(
+                note, 55, -50, 163, 163
+            );
+        };
+    }, [])
 
-    const correctAnswer = ()=>{
+    const correctAnswer = () => {
 
     }
 
-    const wrongAnswer = ()=> {
+    const wrongAnswer = () => {
 
     }
     return (
         <div id="note-identification" className="display-note">
             <canvas ref={myCanvas} id="note-display-canvas">
-                
+
             </canvas>
             <div id="note-display">
-                <img className="staff" src={staff} alt=""/>
+                <img className="staff" src={staff} alt="" />
                 <img className="wholenote" src={wholenote} alt="" />
                 <img className="trebleclef" src={trebleclef} alt="" />
                 <img className="bassclef" src={bassclef} alt="" />
